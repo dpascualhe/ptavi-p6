11bafe813e5e1279a0cec7e35b9183c92068c447
david pascual hernandez <dpascual@iota36.aulas.gsyc.es>
2014-11-17 10:58:05 +0100

    cliente funcionando, servidor responde

--- client.py
+++ client.py
@@ -5,24 +5,55 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+# Comprobamos si tenemos los argumentos correctos
+if len(sys.argv) != 3:
+    print "Usage: python client.py method receiver@IP:SIPport"
+    raise SystemExit
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+SERVER = sys.argv[2].split('@')[1].split(':')[0]
+PORT = int(sys.argv[2].split('@')[1].split(':')[1])
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+# Método requerido
+METHOD = sys.argv[1]
+
+PETICION = METHOD + " sip:" + sys.argv[2] + " SIP/2.0\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+print "Enviando: " + PETICION
+my_socket.send(PETICION + '\r\n')
+try:
+    data = my_socket.recv(1024)
+except socket.error:
+    print "Error: No server listening at 193.147.73.20 port 5555"
+    raise SystemExit    
+
+data = data.split('\r\n\r\n')
+
+i = 0
+"""mess = []
+for line in data:
+    mess.append(line)
+    i += 1"""
+"""
+if mess = ["SIP/2.0 100 Trying", "SIP/2.0 180 Ring", "SIP/2.0 200 OK"]:
+    my_socket.send("ACK sip:" + sys.argv[2] + " SIP/2.0\r\n" + '\r\n')
+elif mess = ["SIP/2.0 400 Bad Request"]:
+    print "El servidor no entiende la petición"
+elif mess = ["SIP/2.0 405 Method Not Allowed"]:
+    print "El servidor no entiende el método requerido"
+"""
+print data
+if data == ["Hemos recibido tu peticion"]:
+    PETICION = "ACK sip:" + sys.argv[2] + " SIP/2.0\r\n"
+    my_socket.send(PETICION + '\r\n')
+    print "Enviando: " + PETICION
 
 print 'Recibido -- ', data
 print "Terminando socket..."
--- server.py
+++ server.py
@@ -5,7 +5,19 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
 
+# Comprobamos si los argumentos son válidos
+if len(sys.argv) != 4:
+    print "Usage: python server.py IP port audio_file"
+    raise SystemExit
+"""
+elif sys.argv[4] does not exist:
+    print "Usage: python server.py IP port audio_file"
+    raise SystemExit
+"""
+
+accepted = ['INVITE', 'ACK', 'BYE']
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -13,16 +25,14 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print "El cliente nos manda " + line
+        line = self.rfile.read()
+        print "El cliente nos manda " + line
+        word = line.split(' ')
 
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        # Envia los códigos de respuesta correspondientes
+        if word[0] in accepted and word[2] == 'SIP/2.0\r\n\r\n':
+            respuesta = "SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n"
+            self.wfile.write(respuesta)
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
